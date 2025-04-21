62bf494b2699a58e45fe1037c851fb40da604e15
Anaslbkh <anasdhj608@gmail.com>
2025-04-21 23:38:58 +0100

    change the sections

--- components/nav.tsx
+++ components/nav.tsx
@@ -4,8 +4,10 @@ export default function Nav() {
         <div className="text-lg font-bold flex-1/2"><span>AL</span><span>.</span></div>
         <ul className="flex center flex-1/2">
             <li><a href="#home" className="hover:text-gray-400">Home</a></li>
-            <li><a href="#about" className="hover:text-gray-400">About</a></li>
-            <li><a href="#services" className="hover:text-gray-400">Services</a></li>
+            <li><a href="#about" className="hover:text-gray-400">Experience</a></li>
+            <li><a href="#services" className="hover:text-gray-400">ME</a></li>
+            <li><a href="#services" className="hover:text-gray-400">Projects</a></li>
+            <li><a href="#services" className="hover:text-gray-400">Vibe coding</a></li>
             <li><a href="#contact" className="hover:text-gray-400">Contact</a></li>
         </ul>
         </nav>

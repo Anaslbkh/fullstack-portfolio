ce59ada6cf3ac7c3e5219afc34e2f85fc06c65bd
Anaslbkh <anasdhj608@gmail.com>
2025-04-22 13:14:58 +0100

    optimize the aimnated text: hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from 'react';
 
 export default function Home() {
   const [currentTextIndex, setCurrentTextIndex] = useState(0);
-  const texts = ["Frontend Developer", "Problem Solver", "Creative Thinker"];
+  const texts = ["Frontend Developer", "Problem Solver", "Creative Thinker","Vibe coder", "Lifelong Learner", "Pixel Perfectionist"];
   const phrases = [
     'Hi,',
     "I'm Anass",
@@ -28,17 +28,27 @@ export default function Home() {
   return (
     <div>
       <Nav />
-    <section className="hero">
-      <div className="glitch-effect"></div>
-      <div className="hero-content">
-        <h1 data-text="Hi, I'm Anass">Hi, I'm Anass</h1>
-        <h2>I'm a <span className="dynamic-text"><span className="text-wrapper">{texts[currentTextIndex]}</span></span></h2>
-        <p>Crafting clean, responsive, and performant web applications.</p>
-        <a href="#contact" className="cta-button">Get in Touch</a>
-      </div>
-      
-    </section>
-    <div className="hero-animation">
+      <section className="hero">
+        <div className="glitch-effect"></div>
+        <div className="hero-content">
+          <h1 data-text="Hi, I'm Anass">Hi, I'm Anass</h1>
+          <h2 className="flex justify-center align-baseline">
+            I'm a{" "}
+            <span className="dynamic-text pl-2.5">
+              <span
+                className="text-wrapper transition-all duration-1000 ease-in-out"
+              >
+                {texts[currentTextIndex]}
+              </span>
+            </span>
+          </h2>
+          <p>Crafting clean, responsive, and performant web applications.</p>
+          <a href="#contact" className="cta-button">
+            Get in Touch
+          </a>
+        </div>
+      </section>
+      <div className="hero-animation">
         <HeroAnimation phrases={phrases} />
       </div>
       <div className="hero-background"></div>
